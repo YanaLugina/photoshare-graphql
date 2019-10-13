dae12c550661fc69863d21f0e00b77a15997ddf8
YanaLugina <yana4961@gmail.com>
2019-10-13 23:41:17 +1000

    Master: was created apollo-server, simple schema GraphQL and resolvers

---
+++ index.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require('apollo-server');
+
+let photos = [];
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean!
+  }
+`;
+
+
+const resolvers = {
+    Query: {
+        totalPhotos: () => photos.length
+    },
+    Mutation: {
+        postPhoto(parent, args) {
+            photos.push(args);
+            return true;
+        }
+    }
+
+};
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server
+    .listen()
+    .then(({url}) => console.log(`GraphQL Server running on ${url}`));
