6e81cb0c0d49e71a4e9763a247047c1443d3bdf4
YanaLugina <yana4961@gmail.com>
2019-10-27 19:14:46 +1000

    apollo: was checked all errors, authorization is successful

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
-const expressPlayground = require('graphql-playground-middleware-express').default;
-const { readFileSync } = require('fs');
+const {ApolloServer} = require('apollo-server-express');
 const {MongoClient} = require('mongodb');
+const {readFileSync} = require('fs');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const resolvers = require('./resolvers/index');
 require('dotenv').config();
 
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
-const resolvers = require('./resolvers');
 
 //  Создаем асинхронную функцию
 async function start() {
-    try {
         const app = express();
         const MONGO_DB = process.env.DB_HOST;
+
         const client = await MongoClient.connect(
             MONGO_DB,
-            {useNewUrlParser: true}
+            {useNewUrlParser: true, useUnifiedTopology: true}
         );
-        const db = client.db();
+        let db = client.db();
+
         const context = {db};
 
 
-
-
-
-        const server = new ApolloServer({typeDefs, resolvers, context});
+        const server = new ApolloServer({
+                typeDefs,
+                resolvers,
+                context
+        });
 
         server.applyMiddleware({app});
 
@@ -35,54 +37,6 @@ async function start() {
         app.listen({port: 4000}, () => {
             console.log(`GraphQL Server @ http://localhost:4000${server.graphqlPath}`)
         });
-    } catch (e) {
-        console.error(e);
-    }
-
 }
 
 start();
-
-
-/*
-let _id = 5;
-
-let tags = [
-    { "photoID": "1", "userID": "gPlake" },
-    { "photoID": "2", "userID": "sSchmidt" },
-    { "photoID": "2", "userID": "mHattrup" },
-    { "photoID": "2", "userID": "gPlake" }
-];
-
-let users = [
-    { "githubLogin": "mHattrup", "name": "Mike Huttrup" },
-    { "githubLogin": "sSchmidt", "name": "Glen Plake" },
-    { "githubLogin": "gPlake", "name": "Scot Schmidt" }
-];
-
-let photos = [
-    {
-        "id": "1",
-        "name": "Dropping the Heart Chute",
-        "description": "the heart chute is one of my favorite chutes",
-        "category": "ACTION",
-        "githubUser": "gPlake",
-        "created": "3-28-1977"
-    },
-    {
-        "id": "2",
-        "name": "Enjoying the sunshine",
-        "category": "SELFIE",
-        "githubUser": "sSchmidt",
-        "created": "1-2-1986"
-    },
-    {
-        "id": "3",
-        "name": "Gunbarrel 25",
-        "description": "25 laps on gunbarrel today",
-        "category": "LANDSCAPE",
-        "githubUser": "sSchmidt",
-        "created": "2018-04-15T19:09:57.308Z"
-    }
-];
-*/
--- lib.js
+++ lib.js
@@ -1,28 +1,31 @@
+const fetch = require('node-fetch');
+
 const requestGithubToken = credentials => {
-    fetch(
-        'https://github.com/login/oauth/access_token',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
+    return fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
         .then(res => res.json())
         .catch(error => {
             throw new Error(JSON.stringify(error));
-        });
+        })
 };
 
 const requestGithubUserAccount = token => {
     return fetch(`https://api.github.com/user?access_token=${token}`)
-        .then(toJSON)
-        .catch(throwError)
+        .then(res => res.json())
 };
 
-async function authorizeWithGithub(credentials) {
+const authorizeWithGithub = async credentials => {
     const {access_token} = await requestGithubToken(credentials);
     const githubUser = await requestGithubUserAccount(access_token);
     return {...githubUser, access_token};
 };
+
+module.exports = {authorizeWithGithub};
